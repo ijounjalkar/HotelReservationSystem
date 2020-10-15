a74dbda03c90ca5c8f7ece77bdf2af83189539fe
ijounjalkar <jounjalkarisha@gmail.com>
2020-10-15 14:52:01 +0530

    Welcome to the Hotel Reservation Program

---
+++ src/main/java/com/HotelReservation/HotelReservation.java
@@ -0,0 +1,9 @@
+package com.HotelReservation;
+
+public class HotelReservation {
+	public static void main( String[] args )
+    {
+        System.out.println( "Welcome to the Hotel Reservation Program" );
+    }
+
+}
